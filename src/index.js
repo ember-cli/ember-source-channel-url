9498aeff09b2afe002ea3d42d2a47c0b7ae36784
Ryan Mark <rgmark@gmail.com>
2018-04-17 16:05:29 -0700

    use caw

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 'use strict';
 
 const got = require('got');
-const proxy = require('./proxy');
-const httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy;
-const noProxy = process.env.NO_PROXY || process.env.no_proxy;
+const caw = require('caw');
 
 module.exports = function(channelType) {
   let HOST = process.env.EMBER_SOURCE_CHANNEL_URL_HOST || 'https://s3.amazonaws.com';
   let PATH = 'builds.emberjs.com';
   let url = `${HOST}/${PATH}/${channelType}.json`;
-  let opts = { json: true };
 
-  if (httpsProxy) {
-    const agent = proxy.proxyAgent(httpsProxy, noProxy, url);
+  let agent = {
+    http: caw({ protocol: 'http' }),
+    https: caw({ protocol: 'https' }),
+  };
 
-    if (agent) {
-      opts.agent = agent;
-    }
-  }
+  let opts = { json: true, agent };
 
   return got(url, opts).then(result => `${HOST}/${PATH}${result.body.assetPath}`);
 };

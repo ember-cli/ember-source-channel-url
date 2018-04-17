8a7bc19d3161750f98485cc561a1027ee4c37598
Ryan Mark <rgmark@gmail.com>
2018-04-17 13:03:28 -0700

    adds proxy auto discovery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 'use strict';
 const got = require('got');
+const proxy = require('./proxy');
+const HOST = process.env.EMBER_SOURCE_CHANNEL_URL_HOST || 'https://s3.amazonaws.com';
+const PATH = 'builds.emberjs.com';
+const httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy;
+const noProxy = process.env.NO_PROXY || process.env.no_proxy;
 
 module.exports = function(channelType) {
-  let HOST = process.env.EMBER_SOURCE_CHANNEL_URL_HOST || 'https://s3.amazonaws.com';
-  let PATH = 'builds.emberjs.com';
+  let url = `${HOST}/${PATH}/${channelType}.json`;
+  let opts = { json: true };
 
-  return got(`${HOST}/${PATH}/${channelType}.json`, { json: true }).then(
-    result => `${HOST}/${PATH}${result.body.assetPath}`
-  );
+  if (httpsProxy) {
+    const agent = proxy.proxyAgent(httpsProxy, noProxy, url);
+
+    if (agent) {
+      opts.agent = agent;
+    }
+  }
+
+  return got(url, opts).then(result => `${HOST}/${PATH}${result.body.assetPath}`);
 };

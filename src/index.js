185a6089115ede7d150849d596c50c005c72bdd7
Ryan Mark <rgmark@gmail.com>
2018-04-17 13:18:54 -0700

    pass test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 'use strict';
 const got = require('got');
 const proxy = require('./proxy');
-const HOST = process.env.EMBER_SOURCE_CHANNEL_URL_HOST || 'https://s3.amazonaws.com';
-const PATH = 'builds.emberjs.com';
 const httpsProxy = process.env.HTTPS_PROXY || process.env.https_proxy;
 const noProxy = process.env.NO_PROXY || process.env.no_proxy;
 
 module.exports = function(channelType) {
+  let HOST = process.env.EMBER_SOURCE_CHANNEL_URL_HOST || 'https://s3.amazonaws.com';
+  let PATH = 'builds.emberjs.com';
   let url = `${HOST}/${PATH}/${channelType}.json`;
   let opts = { json: true };
 

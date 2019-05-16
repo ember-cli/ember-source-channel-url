79c4111dae4fd237043da49077909f86bd92ef33
Robert Jackson <rjackson@linkedin.com>
2019-05-16 11:16:52 -0400

    Refactor to async/await usage.

--- tests/index-test.js
+++ tests/index-test.js
@@ -18,25 +18,24 @@ QUnit.module('ember-source-channel-url', function(hooks) {
     return crypto.randomBytes(Math.ceil(length / 2)).toString('hex');
   }
 
-  hooks.beforeEach(function() {
+  hooks.beforeEach(async function() {
     let dir = tmp.dirSync();
     process.chdir(dir.name);
 
-    return createServer().then(server => {
-      process.env.EMBER_SOURCE_CHANNEL_URL_HOST = `http://localhost:${server.port}`;
+    let server = await createServer();
+    process.env.EMBER_SOURCE_CHANNEL_URL_HOST = `http://localhost:${server.port}`;
 
-      this.server = server;
-      this.fakeSHA = randomString(20);
-      let assetPath = (this.assetPath = `/canary/shas/${this.fakeSHA}.tgz`);
+    this.server = server;
+    this.fakeSHA = randomString(20);
+    let assetPath = (this.assetPath = `/canary/shas/${this.fakeSHA}.tgz`);
 
-      this.expectedURL = `http://${server.host}:${server.port}/builds.emberjs.com${this.assetPath}`;
+    this.expectedURL = `http://${server.host}:${server.port}/builds.emberjs.com${this.assetPath}`;
 
-      server.on('/builds.emberjs.com/canary.json', (req, res) => {
-        res.end(JSON.stringify({ assetPath }));
-      });
-
-      return server.listen(server.port);
+    server.on('/builds.emberjs.com/canary.json', (req, res) => {
+      res.end(JSON.stringify({ assetPath }));
     });
+
+    return server.listen(server.port);
   });
 
   hooks.afterEach(function() {
@@ -45,132 +44,128 @@ QUnit.module('ember-source-channel-url', function(hooks) {
     return this.server.close();
   });
 
-  QUnit.test('works', function(assert) {
+  QUnit.test('works', async function(assert) {
     let expected = `http://${this.server.host}:${this.server.port}/builds.emberjs.com${
       this.assetPath
     }`;
 
-    return getChannelURL('canary').then(actual => {
-      assert.equal(actual, expected);
-    });
+    let actual = await getChannelURL('canary');
+    assert.equal(actual, expected);
   });
 
   QUnit.module('binary', function() {
-    QUnit.test('works', function(assert) {
-      return execa(EXECUTABLE_PATH, ['canary']).then(results => {
-        assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-      });
+    QUnit.test('works', async function(assert) {
+      let results = await execa(EXECUTABLE_PATH, ['canary']);
+      assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
     });
 
-    QUnit.test('when the terminal is not a TTY return only the URL', function(assert) {
+    QUnit.test('when the terminal is not a TTY return only the URL', async function(assert) {
       let file = tmp.fileSync();
-      return execa(EXECUTABLE_PATH, ['canary'], { stdout: file.fd }).then(() => {
-        assert.equal(
-          fs.readFileSync(file.name, { encoding: 'utf8' }),
-          this.expectedURL,
-          'stdout is the URL'
+      await execa(EXECUTABLE_PATH, ['canary'], { stdout: file.fd });
+      assert.equal(
+        fs.readFileSync(file.name, { encoding: 'utf8' }),
+        this.expectedURL,
+        'stdout is the URL'
+      );
+    });
+
+    QUnit.test('updates local package.json when -w is passed (dependencies)', async function(
+      assert
+    ) {
+      fs.writeFileSync(
+        'package.json',
+        JSON.stringify({ dependencies: { 'ember-source': '^3.10.0' } }),
+        { encoding: 'utf8' }
+      );
+
+      let results = await execa(EXECUTABLE_PATH, ['canary', '-w']);
+      assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
+      assert.deepEqual(JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })), {
+        dependencies: {
+          'ember-source': this.expectedURL,
+        },
+      });
+    });
+
+    QUnit.test('updates local package.json when --write is passed (dependencies)', async function(
+      assert
+    ) {
+      fs.writeFileSync(
+        'package.json',
+        JSON.stringify({ dependencies: { 'ember-source': '^3.10.0' } }),
+        { encoding: 'utf8' }
+      );
+
+      let results = await execa(EXECUTABLE_PATH, ['canary', '--write']);
+      assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
+      assert.deepEqual(JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })), {
+        dependencies: {
+          'ember-source': this.expectedURL,
+        },
+      });
+    });
+
+    QUnit.test(
+      'updates local package.json when --write is passed (devDependencies)',
+      async function(assert) {
+        fs.writeFileSync(
+          'package.json',
+          JSON.stringify({ devDependencies: { 'ember-source': '^3.10.0' } }),
+          { encoding: 'utf8' }
         );
-      });
-    });
 
-    QUnit.test('updates local package.json when -w is passed (dependencies)', function(assert) {
-      fs.writeFileSync(
-        'package.json',
-        JSON.stringify({ dependencies: { 'ember-source': '^3.10.0' } }),
-        { encoding: 'utf8' }
-      );
-
-      return execa(EXECUTABLE_PATH, ['canary', '-w']).then(results => {
+        let results = await execa(EXECUTABLE_PATH, ['canary', '--write']);
         assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-
-        assert.deepEqual(JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })), {
-          dependencies: {
-            'ember-source': this.expectedURL,
-          },
-        });
-      });
-    });
-
-    QUnit.test('updates local package.json when --write is passed (dependencies)', function(
-      assert
-    ) {
-      fs.writeFileSync(
-        'package.json',
-        JSON.stringify({ dependencies: { 'ember-source': '^3.10.0' } }),
-        { encoding: 'utf8' }
-      );
-
-      return execa(EXECUTABLE_PATH, ['canary', '--write']).then(results => {
-        assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-
-        assert.deepEqual(JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })), {
-          dependencies: {
-            'ember-source': this.expectedURL,
-          },
-        });
-      });
-    });
-
-    QUnit.test('updates local package.json when --write is passed (devDependencies)', function(
-      assert
-    ) {
-      fs.writeFileSync(
-        'package.json',
-        JSON.stringify({ devDependencies: { 'ember-source': '^3.10.0' } }),
-        { encoding: 'utf8' }
-      );
-
-      return execa(EXECUTABLE_PATH, ['canary', '--write']).then(results => {
-        assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-
         assert.deepEqual(JSON.parse(fs.readFileSync('package.json', { encoding: 'utf8' })), {
           devDependencies: {
             'ember-source': this.expectedURL,
           },
         });
-      });
-    });
+      }
+    );
 
-    QUnit.test('preserves line ending when updating package.json', function(assert) {
+    QUnit.test('preserves line ending when updating package.json', async function(assert) {
       fs.writeFileSync(
         'package.json',
         JSON.stringify({ dependencies: { 'ember-source': '^3.10.0' } }, null, 2) + '\n',
         { encoding: 'utf8' }
       );
 
-      return execa(EXECUTABLE_PATH, ['canary', '--write']).then(results => {
-        assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-
-        let expected =
-          JSON.stringify({ dependencies: { 'ember-source': this.expectedURL } }, null, 2) + '\n';
-        assert.deepEqual(fs.readFileSync('package.json', { encoding: 'utf8' }), expected);
-      });
+      let results = await execa(EXECUTABLE_PATH, ['canary', '--write']);
+      assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
+      let expected =
+        JSON.stringify({ dependencies: { 'ember-source': this.expectedURL } }, null, 2) + '\n';
+      assert.deepEqual(fs.readFileSync('package.json', { encoding: 'utf8' }), expected);
     });
 
-    QUnit.test('fails when package.json is missing', function(assert) {
-      return execa(EXECUTABLE_PATH, ['canary', '--write']).catch(results => {
+    QUnit.test('fails when package.json is missing', async function(assert) {
+      try {
+        await execa(EXECUTABLE_PATH, ['canary', '--write']);
+      } catch (results) {
         assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-
         assert.ok(
           results.stdout.includes('no package.json is available to update'),
           'warning is printed indicating -w failed'
         );
-      });
+      }
     });
 
-    QUnit.test('fails when ember-source is not a dep', function(assert) {
-      fs.writeFileSync('package.json', JSON.stringify({}), { encoding: 'utf8' });
+    QUnit.test('fails when ember-source is not a dep', async function(assert) {
+      fs.writeFileSync('package.json', JSON.stringify({}), {
+        encoding: 'utf8',
+      });
 
-      return execa(EXECUTABLE_PATH, ['canary', '--write']).catch(results => {
+      try {
+        await execa(EXECUTABLE_PATH, ['canary', '--write']);
+      } catch (results) {
         assert.ok(results.stdout.includes(this.expectedURL), 'URL is present in stdout');
-
         assert.ok(
           results.stdout.includes(
             'ember-source is not included in dependencies or devDependencies'
           ),
           'warning is printed indicating -w failed'
         );
-      });
+      }
     });
   });
 });
